f86b80a94f394c61b6df744caa22c1395ed07cf8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-17 15:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(collection){
-  collection.map(function (item) {
+  return collection.map(function (item) {
     return item.toLowerCase();
   });
 }
