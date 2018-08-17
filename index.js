3c746ae9f281bd4c32d56f8f4800f72c7cb45ef6
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-17 15:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function splitName(fullname){
   return fullname.split(" ")
 }
 
-//function nameToAttributes(collection){
-//  return collection.map(function (item) {
+function nameToAttributes(collection){
+  return collection.map(function (item) {
 //    return Object.assign({}, {firstName: splitName(item)[0])}, {lastName: splitName(item)[1])});
-//  });
-//}
+  });
+}
